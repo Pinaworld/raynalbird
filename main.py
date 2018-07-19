eede3b1df30b27f6228edd19c052fd8dabdfcdcf
Pinaworld <pinben02@gmail.com>
2018-07-19 11:38:54 +0200

    windowsAchitecture

--- main.py
+++ main.py
@@ -7,23 +7,34 @@ pygame.init()
 window = pygame.display.set_mode((1280, 720))
 
 pygame.display.set_caption("--FLAPPY RAYNAL--")
-backHome = pygame.image.load("accBack.png").convert_alpha()
-window.blit(backHome, (0,0))
-
-pygame.display.flip()
 
 
-runHome = True
-runGame = True
+run = True
 
-#Accueil
-while runHome:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == K_ESCAPE or event:
-                runHome = False
 
+#RUNNER
+while run:
+
+        runHome = True
+        runGame = True
+
+        backHome = pygame.image.load("accBack.png").convert_alpha()
+        window.blit(backHome, (0,0))
             
-    pygame.display.flip()
+        pygame.display.flip()
+        
+        #HOME
+        while runHome:
+            for event in pygame.event.get():
+                if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+                    runHome = False
+                    run = False
+                    runGame = False
+                    
+                    
+            pygame.display.flip()
+
 
 pygame.quit()
+
+#GAME
