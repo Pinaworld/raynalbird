c2a3887fca111447f19df82a95cf4b48094d2dc8
Pinaworld <pinben02@gmail.com>
2018-07-19 11:57:01 +0200

    menuWindows

--- main.py
+++ main.py
@@ -16,25 +16,42 @@ run = True
 while run:
 
         runHome = True
-        runGame = True
+        runGame = False
 
-        backHome = pygame.image.load("accBack.png").convert_alpha()
-        window.blit(backHome, (0,0))
+        backHome = pygame.image.load("homeBack.png").convert_alpha()
+        backGame = pygame.image.load("gameBack.png").convert_alpha()
             
         pygame.display.flip()
         
         #HOME
         while runHome:
+            window.blit(backHome, (0,0))
             for event in pygame.event.get():
                 if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
                     runHome = False
                     run = False
                     runGame = False
-                    
+                    print("QuitOnHome")
+                if event.type == KEYDOWN and event.key != K_ESCAPE:
+                    print("RunningGame")
+                    runGame = True
+                    runHome = False
+            pygame.display.flip()
+            
+        #GAME
+        while runGame:
+            window.blit(backGame, (0, 0))
+            
+            for event in pygame.event.get():
+                if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+                    runHome = True
+                    runGame = False
+                    print("GameToHome")
                     
             pygame.display.flip()
 
 
+            
 pygame.quit()
 
 #GAME
